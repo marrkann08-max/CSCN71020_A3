b6659654a0db150a7b9172f2acb7fc7980121dd7
Keshav Markan <marrkann08@gmail.com>
2026-03-15 18:23:34 -0400

    Implemented playGame function to pass all tests

--- CSCN71020_A3.starter (2)/A3/Game/game.c
+++ CSCN71020_A3.starter (2)/A3/Game/game.c
@@ -5,10 +5,34 @@
 
 const char* playGame(const char* player1Shape, const char* player2Shape)
 {
-	//#write functional code here
-	return STR_INVALID;
-}
+    // check invalid inputs
+    if ((strcmp(player1Shape, STR_ROCK) != 0 &&
+        strcmp(player1Shape, STR_PAPER) != 0 &&
+        strcmp(player1Shape, STR_SISSOR) != 0) ||
+        (strcmp(player2Shape, STR_ROCK) != 0 &&
+            strcmp(player2Shape, STR_PAPER) != 0 &&
+            strcmp(player2Shape, STR_SISSOR) != 0))
+    {
+        return STR_INVALID;
+    }
 
+    // check draw
+    if (strcmp(player1Shape, player2Shape) == 0)
+    {
+        return STR_DRAW;
+    }
+
+    // player1 winning conditions
+    if ((strcmp(player1Shape, STR_ROCK) == 0 && strcmp(player2Shape, STR_SISSOR) == 0) ||
+        (strcmp(player1Shape, STR_SISSOR) == 0 && strcmp(player2Shape, STR_PAPER) == 0) ||
+        (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_ROCK) == 0))
+    {
+        return STR_PLAYER1;
+    }
+
+    // otherwise player2 wins
+    return STR_PLAYER2;
+}
 int main()
 {
     return 1;
--- CSCN71020_A3.starter (2)/A3/UnitTestDriver/UnitTestDriver.cpp
+++ CSCN71020_A3.starter (2)/A3/UnitTestDriver/UnitTestDriver.cpp
@@ -10,10 +10,60 @@ namespace UnitTestDriver
 	{
 	public:
 
-		//// write your test cases here
-		TEST_METHOD(TestMethod1)
-		{
-			Assert::AreEqual(STR_DRAW, playGame(STR_ROCK, STR_ROCK));
-		}
+
+        TEST_METHOD(DrawCase_Rock_vs_Rock)
+        {
+            Assert::AreEqual(STR_DRAW, playGame(STR_ROCK, STR_ROCK));
+        }
+
+        TEST_METHOD(DrawCase_Paper_vs_Paper)
+        {
+            Assert::AreEqual(STR_DRAW, playGame(STR_PAPER, STR_PAPER));
+        }
+
+        TEST_METHOD(DrawCase_Sissor_vs_Sissor)
+        {
+            Assert::AreEqual(STR_DRAW, playGame(STR_SISSOR, STR_SISSOR));
+        }
+
+        TEST_METHOD(Player1Wins_Rock_over_Sissor)
+        {
+            Assert::AreEqual(STR_PLAYER1, playGame(STR_ROCK, STR_SISSOR));
+        }
+
+        TEST_METHOD(Player1Wins_Sissor_over_Paper)
+        {
+            Assert::AreEqual(STR_PLAYER1, playGame(STR_SISSOR, STR_PAPER));
+        }
+
+        TEST_METHOD(Player1Wins_Paper_over_Rock)
+        {
+            Assert::AreEqual(STR_PLAYER1, playGame(STR_PAPER, STR_ROCK));
+        }
+
+        TEST_METHOD(Player2Wins_Rock_over_Sissor)
+        {
+            Assert::AreEqual(STR_PLAYER2, playGame(STR_SISSOR, STR_ROCK));
+        }
+
+        TEST_METHOD(Player2Wins_Sissor_over_Paper)
+        {
+            Assert::AreEqual(STR_PLAYER2, playGame(STR_PAPER, STR_SISSOR));
+        }
+
+        TEST_METHOD(Player2Wins_Paper_over_Rock)
+        {
+            Assert::AreEqual(STR_PLAYER2, playGame(STR_ROCK, STR_PAPER));
+        }
+
+        TEST_METHOD(InvalidInput_Player1Shape)
+        {
+            Assert::AreEqual(STR_INVALID, playGame("Gun", STR_ROCK));
+        }
+
+        TEST_METHOD(InvalidInput_Player2Shape)
+        {
+            Assert::AreEqual(STR_INVALID, playGame(STR_ROCK, "Gun"));
+        }
 	};
 }
